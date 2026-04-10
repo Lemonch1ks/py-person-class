9a7a2664124e137e2311e4ccc3097ef7910cbeab
Oleksandr Dvirko <dvalex02@gmail.com>
2026-04-10 18:03:18 +0300

    added 2 funcs

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
+from unittest import result
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
+
+
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    new_peoples = []
+
+    for pers in people:
+        new_peoples.append(Person(name = pers.get("name"), age = pers.get("age")))
+
+    for person in people:
+        current_person = Person.people[person["name"]]
+
+        if person.get("wife") is not None:
+            current_person.wife = Person.people[person["wife"]]
+
+        if person.get("husband") is not None:
+            current_person.husband = Person.people[person["husband"]]
+
+
+
+    return  new_peoples

238ebab73291f8a05f0f13680f21e33775cb98d7
Oleksandr Dvirko <dvalex02@gmail.com>
2026-04-10 18:14:49 +0300

    Fixed for luke

--- app/main.py
+++ app/main.py
@@ -10,16 +10,16 @@ class Person:
 def create_person_list(people: list) -> list:
     new_peoples = []
 
-    for pers in people:
-        new_peoples.append(Person(name=pers.get("name"), age=pers.get("age")))
+    for p in people:
+        new_peoples = [Person(name=p.get("name"), age=p.get("age")) for p in people]
 
-    for person in people:
-        current_person = Person.people[person["name"]]
+    for person_data in people:
+        current_person = Person.people[person_data.get("name")]
 
-        if person.get("wife") is not None:
-            current_person.wife = Person.people[person["wife"]]
+        if person_data.get("wife") is not None:
+            current_person.wife = Person.people[person_data.get("wife")]
 
-        if person.get("husband") is not None:
-            current_person.husband = Person.people[person["husband"]]
+        if person_data.get("husband") is not None:
+            current_person.husband = Person.people[person_data.get("husband")]
 
     return new_peoples
